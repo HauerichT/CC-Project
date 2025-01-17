04a0ef3ab886928876d4a7af068f4b6d0a64aa9d
HauerichT <timo.haverich@fh-bielefeld.de>
2025-01-17 21:48:26 +0100

    ADD debugging.

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -50,7 +50,7 @@ router.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   console.log("EMAIL PASSWORD: ", email, password);
   try {
